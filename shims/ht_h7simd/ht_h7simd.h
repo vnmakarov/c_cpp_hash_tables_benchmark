b3321185da3f18ac3d691680b056b37edf54f941
Vladimir N. Makarov <vmakarov@redhat.com>
2026-05-09 16:45:07 -0400

    Add simd code for neon in ht_h7simd.h.

--- shims/ht_h7simd/ht_h7simd.h
+++ shims/ht_h7simd/ht_h7simd.h
@@ -5,7 +5,18 @@
 #include <cstdint>
 #include <cstring>
 #include <cassert>
+
+#if defined(__x86_64__) || defined(_M_X64) || defined(__i386__) || defined(_M_IX86)
 #include <immintrin.h>
+#define HT_H7SIMD_USE_SSE2 1
+static constexpr unsigned int HT_H7SIMD_GROUP_SIZE = 16;
+#elif defined(__aarch64__) || defined(_M_ARM64)
+#include <arm_neon.h>
+#define HT_H7SIMD_USE_NEON 1
+static constexpr unsigned int HT_H7SIMD_GROUP_SIZE = 8;
+#else
+#error "ht_h7simd requires SSE2 or NEON"
+#endif
 
 typedef uint32_t ht_h7simd_ind_t;
 typedef unsigned long ht_h7simd_size_t;
@@ -13,9 +24,50 @@ typedef size_t ht_h7simd_hash_t;
 
 static constexpr unsigned char HT_H7SIMD_EMPTY_H7 = 0x80;
 static constexpr unsigned char HT_H7SIMD_DELETED_H7 = 0xfe;
-static constexpr unsigned int HT_H7SIMD_GROUP_SIZE = 16;
 
 enum ht_h7simd_action { HT_H7SIMD_FIND, HT_H7SIMD_INSERT, HT_H7SIMD_REPLACE, HT_H7SIMD_DELETE };
+
+struct ht_h7simd_group {
+  unsigned int match_mask;
+  unsigned int empty_mask;
+};
+
+__attribute__((always_inline))
+static inline ht_h7simd_group ht_h7simd_probe_group (const unsigned char *group_h7,
+                                                      unsigned char h7_val) {
+#if HT_H7SIMD_USE_SSE2
+  __m128i group = _mm_load_si128 ((const __m128i *) group_h7);
+  __m128i h7_vec = _mm_set1_epi8 ((char) h7_val);
+  __m128i empty_vec = _mm_set1_epi8 ((char) HT_H7SIMD_EMPTY_H7);
+  return {
+    (unsigned int) _mm_movemask_epi8 (_mm_cmpeq_epi8 (group, h7_vec)),
+    (unsigned int) _mm_movemask_epi8 (_mm_cmpeq_epi8 (group, empty_vec))
+  };
+#elif HT_H7SIMD_USE_NEON
+  uint8x8_t group = vld1_u8 (group_h7);
+  uint8x8_t match_eq = vceq_u8 (group, vdup_n_u8 (h7_val));
+  uint8x8_t empty_eq = vceq_u8 (group, vdup_n_u8 (HT_H7SIMD_EMPTY_H7));
+  static const uint8x8_t bit_mask = {1, 2, 4, 8, 16, 32, 64, 128};
+  return {
+    (unsigned int) vaddv_u8 (vand_u8 (match_eq, bit_mask)),
+    (unsigned int) vaddv_u8 (vand_u8 (empty_eq, bit_mask))
+  };
+#endif
+}
+
+__attribute__((always_inline))
+static inline unsigned int ht_h7simd_deleted_mask (const unsigned char *group_h7) {
+#if HT_H7SIMD_USE_SSE2
+  __m128i group = _mm_load_si128 ((const __m128i *) group_h7);
+  __m128i del_vec = _mm_set1_epi8 ((char) HT_H7SIMD_DELETED_H7);
+  return (unsigned int) _mm_movemask_epi8 (_mm_cmpeq_epi8 (group, del_vec));
+#elif HT_H7SIMD_USE_NEON
+  uint8x8_t group = vld1_u8 (group_h7);
+  uint8x8_t del_eq = vceq_u8 (group, vdup_n_u8 (HT_H7SIMD_DELETED_H7));
+  static const uint8x8_t bit_mask = {1, 2, 4, 8, 16, 32, 64, 128};
+  return (unsigned int) vaddv_u8 (vand_u8 (del_eq, bit_mask));
+#endif
+}
 
 template<typename El>
 struct hbin_h7simd_t {
@@ -43,7 +95,7 @@ struct ht_h7simd_t {
     b.els = (El *) std::malloc (size * sizeof (El));
     ht_h7simd_size_t del_bytes = (size + 7) / 8;
     b.deleted = (char *) std::calloc (del_bytes, 1);
-    b.h7 = (unsigned char *) std::aligned_alloc (16, entries_size);
+    b.h7 = (unsigned char *) std::aligned_alloc (HT_H7SIMD_GROUP_SIZE, entries_size);
     std::memset (b.h7, HT_H7SIMD_EMPTY_H7, entries_size);
     b.entries = (ht_h7simd_ind_t *) std::malloc (entries_size * sizeof (ht_h7simd_ind_t));
     b.groups_mask = entries_size / HT_H7SIMD_GROUP_SIZE - 1;
@@ -69,18 +121,13 @@ struct ht_h7simd_t {
     ht_h7simd_hash_t hash = hash_fn (el);
     unsigned char h7_val = (hash >> (sizeof (size_t) * 8 - 7)) & 0x7f;
     ht_h7simd_size_t group_ind = (hash / HT_H7SIMD_GROUP_SIZE) & bin.groups_mask;
-
-    __m128i h7_vec = _mm_set1_epi8 ((char) h7_val);
-    __m128i empty_vec = _mm_set1_epi8 ((char) HT_H7SIMD_EMPTY_H7);
     ht_h7simd_size_t first_deleted_slot = ~(ht_h7simd_size_t) 0;
 
     for (;;) {
       unsigned char *group_h7 = bin.h7 + group_ind * HT_H7SIMD_GROUP_SIZE;
-      __m128i group = _mm_load_si128 ((const __m128i *) group_h7);
+      ht_h7simd_group g = ht_h7simd_probe_group (group_h7, h7_val);
 
-      unsigned int match_mask = (unsigned int) _mm_movemask_epi8 (_mm_cmpeq_epi8 (group, h7_vec));
-      unsigned int empty_mask = (unsigned int) _mm_movemask_epi8 (_mm_cmpeq_epi8 (group, empty_vec));
-
+      unsigned int match_mask = g.match_mask;
       while (match_mask) {
         unsigned int bit = __builtin_ctz (match_mask);
         ht_h7simd_size_t slot = group_ind * HT_H7SIMD_GROUP_SIZE + bit;
@@ -98,14 +145,14 @@ struct ht_h7simd_t {
         match_mask &= match_mask - 1;
       }
 
-      if (empty_mask) {
+      if (g.empty_mask) {
         if (action == HT_H7SIMD_INSERT || action == HT_H7SIMD_REPLACE) {
           htab->els_num++;
           ht_h7simd_size_t slot;
           if (first_deleted_slot != ~(ht_h7simd_size_t) 0) {
             slot = first_deleted_slot;
           } else {
-            unsigned int bit = __builtin_ctz (empty_mask);
+            unsigned int bit = __builtin_ctz (g.empty_mask);
             slot = group_ind * HT_H7SIMD_GROUP_SIZE + bit;
           }
           bin.h7[slot] = h7_val;
@@ -117,8 +164,7 @@ struct ht_h7simd_t {
       }
 
       if (first_deleted_slot == ~(ht_h7simd_size_t) 0) {
-        __m128i del_vec = _mm_set1_epi8 ((char) HT_H7SIMD_DELETED_H7);
-        unsigned int del_mask = (unsigned int) _mm_movemask_epi8 (_mm_cmpeq_epi8 (group, del_vec));
+        unsigned int del_mask = ht_h7simd_deleted_mask (group_h7);
         if (del_mask) {
           unsigned int bit = __builtin_ctz (del_mask);
           first_deleted_slot = group_ind * HT_H7SIMD_GROUP_SIZE + bit;
@@ -142,7 +188,7 @@ struct ht_h7simd_t {
       resize_bin.els = (El *) std::malloc (els_size * sizeof (El));
       ht_h7simd_size_t del_bytes = (els_size + 7) / 8;
       resize_bin.deleted = (char *) std::calloc (del_bytes, 1);
-      resize_bin.h7 = (unsigned char *) std::aligned_alloc (16, entries_size);
+      resize_bin.h7 = (unsigned char *) std::aligned_alloc (HT_H7SIMD_GROUP_SIZE, entries_size);
       std::memset (resize_bin.h7, HT_H7SIMD_EMPTY_H7, entries_size);
       resize_bin.entries = (ht_h7simd_ind_t *) std::malloc (entries_size * sizeof (ht_h7simd_ind_t));
       resize_bin.groups_mask = entries_size / HT_H7SIMD_GROUP_SIZE - 1;
